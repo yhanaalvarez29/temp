7a99cf576efd2d82b30a3c9dd9456e70681ffb4e
yhanaalvarez29 <yhanaalvarez997@gmail.com>
2025-05-18 02:46:18 +0800

    Update generate-email.js

--- api/generate-email.js
+++ api/generate-email.js
@@ -1,21 +1,19 @@
-const { TempMail } = require('1secmail-api');
+const axios = require('axios');
 
-function generateRandomId() {
-    var length = 6;
-    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var randomId = '';
-    for (var i = 0; i < length; i++) {
-        randomId += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return randomId;
-}
-
-export default async (req, res) => {
-    try {
-        const mail = new TempMail(generateRandomId());
-        mail.autoFetch();
-        res.json({ email: mail.address });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://api.temp-mail.io/v1/messages/:id/source_code',
+  headers: { 
+    'Accept': 'application/json', 
+    'X-API-Key': '<X-API-Key>'
+  }
 };
+
+axios.request(config)
+.then((response) => {
+  console.log(JSON.stringify(response.data));
+})
+.catch((error) => {
+  console.log(error);
+});
